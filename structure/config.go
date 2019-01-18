599c515c3b3102638c76613cf3a6a295f4ee0af7
ykomissarov <lilipp1q@yandex.ru>
2019-01-18 18:29:09 +0300

    add config schemas for std clients

--- structure/config.go
+++ structure/config.go
@@ -6,9 +6,9 @@ type MetricAddress struct {
 }
 
 type MetricConfiguration struct {
-	Address                MetricAddress `json:"address"`
-	Gc                     bool          `json:"gc"`
-	CollectingGCPeriod     int32         `json:"collectingGCPeriod"`
-	Memory                 bool          `json:"memory"`
-	CollectingMemoryPeriod int32         `json:"collectingMemoryPeriod"`
+	Address                MetricAddress `json:"address" schema:"Metric HTTP server"`
+	Gc                     bool          `json:"gc" schema:"Collect garbage collecting statistic"`
+	CollectingGCPeriod     int32         `json:"collectingGCPeriod" schema:"GC stat collecting interval,In seconds, default: 10"`
+	Memory                 bool          `json:"memory" schema:"Collect memory statistic"`
+	CollectingMemoryPeriod int32         `json:"collectingMemoryPeriod" schema:"Memory stat collecting interval,In seconds, default: 10"`
 }
