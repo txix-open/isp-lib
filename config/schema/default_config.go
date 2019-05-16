580ee755569d31f2b574568b6c57ac6b4bed0f0d
ykomissarov <merser1q@gmail.com>
2019-05-16 14:10:16 +0300

    fix resolving

--- config/schema/default_config.go
+++ config/schema/default_config.go
@@ -26,8 +26,14 @@ func ExtractConfig(path string) (map[string]interface{}, error) {
 }
 
 func ResolveDefaultConfigPath(path string) string {
+	if p.IsAbs(path) {
+		return path
+	}
 	if utils.DEV {
 		return p.Join("conf", path)
+	} else {
+		ex, _ := os.Executable()
+		dir := p.Dir(ex)
+		return p.Join(dir, path)
 	}
-	return path
 }
