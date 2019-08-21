d8ea213ec2182662432624bf09cf6726b56f12c4
merzhanovmv <maksimmerzh@gmail.com>
2019-08-21 16:59:17 +0300

    add DereferenceSchema method

--- config/schema/schema2.go
+++ config/schema/schema2.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"github.com/integration-system/jsonschema"
+	"strings"
 )
 
 const (
@@ -26,4 +27,62 @@ func GenerateConfigSchema(cfgPtr interface{}) Schema {
 	s.Title = "Remote config"
 	s.Version = ""
 	return s
+}
+
+func DereferenceSchema(s Schema) Schema {
+	for k, def := range s.Definitions {
+		s.Definitions[k] = replaceRef(def, s.Definitions)
+	}
+
+	s.Type = replaceRef(s.Type, s.Definitions)
+	return s
+}
+
+func replaceRef(def *jsonschema.Type, definitions jsonschema.Definitions) *jsonschema.Type {
+	if def != nil {
+		if def.Ref != "" {
+			tp := strings.TrimPrefix(def.Ref, "#/definitions/")
+			def = definitions[tp]
+		}
+		if def.Items != nil && def.Items.Ref != "" {
+			tp := strings.TrimPrefix(def.Items.Ref, "#/definitions/")
+			def.Items = definitions[tp]
+		}
+		for k, patternProp := range def.PatternProperties {
+			if patternProp.Ref != "" {
+				tp := strings.TrimPrefix(patternProp.Ref, "#/definitions/")
+				def.PatternProperties[k] = definitions[tp]
+			}
+		}
+		for k, dep := range def.Dependencies {
+			if dep.Ref != "" {
+				tp := strings.TrimPrefix(dep.Ref, "#/definitions/")
+				def.Dependencies[k] = definitions[tp]
+			}
+		}
+		for k, v := range def.AllOf {
+			if v.Ref != "" {
+				tp := strings.TrimPrefix(v.Ref, "#/definitions/")
+				def.AllOf[k] = definitions[tp]
+			}
+		}
+		for k, v := range def.AnyOf {
+			if v.Ref != "" {
+				tp := strings.TrimPrefix(v.Ref, "#/definitions/")
+				def.AnyOf[k] = definitions[tp]
+			}
+		}
+		for k, v := range def.OneOf {
+			if v.Ref != "" {
+				tp := strings.TrimPrefix(v.Ref, "#/definitions/")
+				def.OneOf[k] = definitions[tp]
+			}
+		}
+		replaceRef(def.Not, definitions)
+		replaceRef(def.Media, definitions)
+		for k, prop := range def.Properties {
+			def.Properties[k] = replaceRef(prop, definitions)
+		}
+	}
+	return def
 }
---
+++ config/schema/schema2_test.go
@@ -0,0 +1,66 @@
+package schema
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/integration-system/jsonschema"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGenerateConfigSchema(t *testing.T) {
+	assert := assert.New(t)
+	type Obj3 struct {
+		C string
+	}
+	type Obj2 struct {
+		B    string
+		Obj3 Obj3
+	}
+	type Obj struct {
+		Str  string
+		Obj2 Obj2
+	}
+	type Config struct {
+		A             string
+		B             int
+		C             bool
+		SliceObj3     []Obj3
+		MapStringObj3 map[string]Obj3
+		D             Obj
+	}
+
+	config := Config{
+		A: "test1",
+		B: 1,
+		C: true,
+		D: Obj{
+			Str:  "test1",
+			Obj2: Obj2{B: "bstring", Obj3: Obj3{C: "somestr"}},
+		},
+		SliceObj3: []Obj3{{C: "test"}},
+		MapStringObj3: map[string]Obj3{
+			"s1": {C: "test1"},
+			"s2": {C: "test2"},
+		},
+	}
+	ref := jsonschema.Reflector{
+		FieldNameReflector: GetNameAndRequiredFlag,
+		FieldReflector:     SetProperties,
+		ExpandedStruct:     true,
+	}
+	want := Schema(ref.Reflect(config))
+	got := DereferenceSchema(ref.Reflect(config))
+
+	want.Definitions["Obj2"].Properties["obj3"] = want.Definitions["Obj3"]
+	want.Definitions["Obj"].Properties["obj2"] = want.Definitions["Obj2"]
+	want.Type.Properties["sliceObj3"].Items = want.Definitions["Obj3"]
+	want.Type.Properties["mapStringObj3"].PatternProperties[".*"] = want.Definitions["Obj3"]
+	want.Type.Properties["d"] = want.Definitions["Obj"]
+
+	if !assert.Equal(want, got) {
+		json1, _ := json.MarshalIndent(got, "", "\t")
+		fmt.Println(string(json1))
+	}
+
+}
