11002a9e4c7e71c029ab4c831bd3593697e43178
DeniskaRediska <grj.den@gmail.com>
2019-08-27 15:11:45 +0300

    fix(stack overflow)

--- config/schema/schema2.go
+++ config/schema/schema2.go
@@ -32,14 +32,22 @@ func GenerateConfigSchema(cfgPtr interface{}) Schema {
 
 func DereferenceSchema(s Schema) Schema {
 	cache := make(jsonschema.Definitions)
-	s.Type = dereferenceType(s.Type, s.Definitions, cache)
+	s.Type = dereferenceType(s.Type, s.Definitions, cache, make(map[string]bool))
 	s.Definitions = nil
 	return s
 }
 
-func dereferenceType(t *jsonschema.Type, definitions, cache jsonschema.Definitions) *jsonschema.Type {
+func dereferenceType(t *jsonschema.Type, definitions, cache jsonschema.Definitions, depthMap map[string]bool) *jsonschema.Type {
 	toDeref := t
 	ref := strings.TrimPrefix(t.Ref, "#/definitions/")
+	if ref != "" {
+		if _, ok := depthMap[ref]; ok {
+			return t
+		} else {
+			depthMap[ref] = true
+			defer delete(depthMap, ref)
+		}
+	}
 	if ref != "" {
 		if dereferenced, ok := cache[ref]; ok {
 			copied := *dereferenced
@@ -58,20 +66,20 @@ func dereferenceType(t *jsonschema.Type, definitions, cache jsonschema.Definitio
 	}
 
 	if toDeref.Items != nil {
-		toDeref.Items = dereferenceType(toDeref.Items, definitions, cache)
+		toDeref.Items = dereferenceType(toDeref.Items, definitions, cache, depthMap)
 	}
 	if toDeref.AdditionalItems != nil {
-		toDeref.AdditionalItems = dereferenceType(toDeref.AdditionalItems, definitions, cache)
+		toDeref.AdditionalItems = dereferenceType(toDeref.AdditionalItems, definitions, cache, depthMap)
 	}
 	if toDeref.Not != nil {
-		toDeref.Not = dereferenceType(toDeref.Not, definitions, cache)
+		toDeref.Not = dereferenceType(toDeref.Not, definitions, cache, depthMap)
 	}
-	toDeref.Properties = derefMap(toDeref.Properties, definitions, cache)
-	toDeref.PatternProperties = derefMap(toDeref.PatternProperties, definitions, cache)
-	toDeref.Dependencies = derefMap(toDeref.Dependencies, definitions, cache)
-	toDeref.OneOf = derefArray(toDeref.OneOf, definitions, cache)
-	toDeref.AllOf = derefArray(toDeref.AllOf, definitions, cache)
-	toDeref.AnyOf = derefArray(toDeref.AnyOf, definitions, cache)
+	toDeref.Properties = derefMap(toDeref.Properties, definitions, cache, depthMap)
+	toDeref.PatternProperties = derefMap(toDeref.PatternProperties, definitions, cache, depthMap)
+	toDeref.Dependencies = derefMap(toDeref.Dependencies, definitions, cache, depthMap)
+	toDeref.OneOf = derefArray(toDeref.OneOf, definitions, cache, depthMap)
+	toDeref.AllOf = derefArray(toDeref.AllOf, definitions, cache, depthMap)
+	toDeref.AnyOf = derefArray(toDeref.AnyOf, definitions, cache, depthMap)
 
 	if ref != "" {
 		cache[ref] = toDeref
@@ -86,16 +94,16 @@ func dereferenceType(t *jsonschema.Type, definitions, cache jsonschema.Definitio
 	return toDeref
 }
 
-func derefArray(arr []*jsonschema.Type, definitions, cache jsonschema.Definitions) []*jsonschema.Type {
+func derefArray(arr []*jsonschema.Type, definitions, cache jsonschema.Definitions, depthMap map[string]bool) []*jsonschema.Type {
 	for i, t := range arr {
-		arr[i] = dereferenceType(t, definitions, cache)
+		arr[i] = dereferenceType(t, definitions, cache, depthMap)
 	}
 	return arr
 }
 
-func derefMap(m map[string]*jsonschema.Type, definitions, cache jsonschema.Definitions) map[string]*jsonschema.Type {
+func derefMap(m map[string]*jsonschema.Type, definitions, cache jsonschema.Definitions, depthMap map[string]bool) map[string]*jsonschema.Type {
 	for key, t := range m {
-		m[key] = dereferenceType(t, definitions, cache)
+		m[key] = dereferenceType(t, definitions, cache, depthMap)
 	}
 	return m
 }
