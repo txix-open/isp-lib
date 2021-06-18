487818838471682ff7535f6edf7a3baa65749f1a
Maxim Merzhanov <maksimmerzh@gmail.com>
2021-06-18 14:40:11 +0300

    database: set dial, read, write timeouts

--- database/connection.go
+++ database/connection.go
@@ -9,6 +9,8 @@ import (
 	"github.com/integration-system/isp-lib/v2/structure"
 )
 
+const dialTimeout = 5 * time.Second
+
 func NewDbConnection(config structure.DBConfiguration) (*pg.DB, error) {
 	pdb := pg.Connect(&pg.Options{
 		User:               config.Username,
@@ -18,6 +20,9 @@ func NewDbConnection(config structure.DBConfiguration) (*pg.DB, error) {
 		MaxRetries:         5,
 		IdleTimeout:        time.Duration(15) * time.Minute,
 		IdleCheckFrequency: time.Duration(30) * time.Second,
+		DialTimeout:        dialTimeout,
+		ReadTimeout:        20 * time.Second,
+		WriteTimeout:       20 * time.Second,
 		PoolSize:           config.PoolSize,
 	})
 
@@ -29,13 +34,14 @@ func NewDbConnection(config structure.DBConfiguration) (*pg.DB, error) {
 
 func openSqlConn(config structure.DBConfiguration, schema string) (*sql.DB, error) {
 	cs := fmt.Sprintf(
-		"postgres://%s:%s/%s?search_path=%s,public&sslmode=disable&user=%s&password=%s",
+		"postgres://%s:%s/%s?search_path=%s,public&sslmode=disable&user=%s&password=%s&connect_timeout=%d",
 		config.Address,
 		config.Port,
 		config.Database,
 		schema,
 		config.Username,
 		config.Password,
+		int64(dialTimeout.Seconds()),
 	)
 	return sql.Open("postgres", cs)
 }
